41c43aa166418efbd8911b4382097cce690fc3e6
Pranjeet Dhanapune <pranjeetdhanapune@gmail.com>
2020-09-08 15:53:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import {useStateValue} from "./StateProvider";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  
+  const [{user, token}, dispatch ] = useStateValue();
+
+  //Run code based on a given condition
+  //[] this is to run the code once
+  //we can put variables in brackets so as to run it again when that coressponding  varibles changes its value
+  useEffect( () => {
+    const hash = getTokenFromUrl();
+    window.location.hash="";
+
+    const _token = hash.access_token;
+    if(_token){
+
+      dispatch({
+        type:"SET_TOKEN",
+        token: _token,
+      });
+    
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) =>{
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist("4bhO2RBDd0IxckwNG4GZe0").then((response) => {
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response,
+      });
+    });
+
+    spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+  }
+    
+  }, [token,dispatch]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      { token ? <Player spotify={spotify}/> : <Login />}
     </div>
   );
 }
